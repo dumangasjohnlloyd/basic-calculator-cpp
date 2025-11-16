d9243e7e6e9ef4c0e8c0600588d1cab2e27c3286
dumangasjohnlloyd <dumangasjohnlloyd@gmail.com>
2025-11-16 21:11:41 +0800

    Initial commit - Basic Calculator

---
+++ BasicCalculator.cpp
@@ -0,0 +1,107 @@
+//1st C++ Project: Basic Calculator (BaCa)
+
+#include <iostream>
+using namespace std;
+
+void showMenu();
+void spacing1();
+
+void getNumbers(float &num1, float &num2);
+float multiply(float num1, float num2);
+float divide(float num1, float num2);
+float add(float num1, float num2);
+float subtract(float num1, float num2);
+
+int main() {
+    int selection;
+    float num1, num2, result;
+
+    while (true){
+        showMenu();
+        spacing1();
+
+        cout << "Select an operation: ";
+        cin >> selection;
+
+        cout << endl;
+
+        if (selection == 5) {
+            cout << "\033[34m Exiting program...\033[0m";
+            spacing1();
+            break;
+        } else if ( selection < 1 || selection > 5) {
+            cout << "\033[31m Invalid selection. Please try again!\033[0m";
+            spacing1();
+            continue;
+        }
+    
+        // Get user numbers only once
+
+        getNumbers(num1, num2);
+
+        switch (selection){
+            case 1:
+                result = multiply(num1, num2);
+                cout << endl;
+                cout << num1 << " * " << num2 << " = " << result;
+                spacing1();
+                break;
+
+            case 2:
+                result = divide(num1, num2);
+                cout << endl;
+                cout << num1 << " / " << num2 << " = " << result;
+                spacing1();
+                break;
+
+            case 3:
+                result = add(num1, num2);
+                cout << endl;
+                cout << "\033[32m" << num1 << " + " << num2 << " = " << result << "\033[0m";
+                spacing1();
+                break;
+                
+            case 4:
+                result = subtract(num1, num2);
+                cout << endl;
+                cout << "\033[32m" << num1 << " - " << num2 << " = " << result << "\033[0m";
+                spacing1();
+                break;
+
+            //lacks type proofing
+
+            default:
+                return 0;
+                break;
+        }
+
+    }
+    return 0;
+}
+
+//FUNCTION DEFINITION
+
+void showMenu() {
+    cout << "****** Basic Calculator ******" << endl;
+    cout << "[1] Multiply" << endl;
+    cout << "[2] Divide" << endl;
+    cout << "[3] Add" << endl;
+    cout << "[4] Subtract" << endl;
+    cout << "[5] Exit";
+}
+
+void spacing1 () {
+    cout << endl << endl;
+}
+
+void getNumbers(float &num1, float &num2) {
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+}
+
+float multiply(float num1, float num2) { return  num1 * num2; }
+float divide( float num1, float num2) { return num1 / num2; }
+float add(float num1, float num2) { return num1 + num2; }
+float subtract(float num1, float num2) { return num1 - num2; }
